0c67ab219fa5473aad26b01e2cd567b2d8799b79
git-austinmccollom <austinmccollom@gmail.com>
2020-10-16 11:48:50 -0500

    login

--- auth/auth-db-functions.js
+++ auth/auth-db-functions.js
@@ -1,19 +1,29 @@
-const db = require('../database/dbConfig.js');
+const db = require("../database/dbConfig.js");
 
 module.exports = {
-    addUser,
-    findById
-}
+  addUser,
+  findById,
+  findByUsername,
+  getUsers
+};
 
 async function addUser(user) {
-    try {
-        const id = await db("users").insert(user);
-        return findById(id[0]);
-    } catch (error) {
-        throw error;
-    }
+  try {
+    const id = await db("users").insert(user);
+    return findById(id[0]);
+  } catch (error) {
+    throw error;
+  }
 }
 
 function findById(id) {
-    return db("users").where({ id }).first();
+  return db("users").where({ id }).first();
+}
+
+function findByUsername(username) {
+    return db("users").where({ username: username }).first();
   }
+
+function getUsers() {
+    return db("users")
+}
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,28 +1,71 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const dbFun = require('./auth-db-functions');
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const dbFun = require("./auth-db-functions");
 const jwt = require("jsonwebtoken");
-const { jwtSecret } = require("./secrets.js")
+const { jwtSecret } = require("./secrets.js");
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   const credentials = req.body;
   const hash = bcrypt.hashSync(credentials.password, 10);
   credentials.password = hash;
 
-  dbFun.addUser(credentials)
+  dbFun
+    .addUser(credentials)
+    .then((dbRes) => {
+      res.status(200).json(dbRes);
+    })
+    .catch((dbErr) => {
+      res.status(500).json(dbErr);
+    });
+});
+
+function generateToken(user) {
+  const payload = {
+      subject: user.id,
+      username: user.username,
+      role: user.role
+  }
+
+  const options = {
+      expiresIn: '1h'
+  };
+
+  return jwt.sign(payload, jwtSecret, options)
+}
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+  console.log("username", username, "password", password)
+
+  dbFun.findByUsername(username).first()
+  // dbFun.findById(2)
+  .then((user) => {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      // res.status(200).json({ message: `Welcome ${user.username}!` });
+      const token = generateToken(user);
+      res
+        .status(200)
+        .json({
+          message: `Welcome: ${user.username}! you have a token`,
+          token,
+        });
+    } else {
+      res.status(401).json({ error: "You shall not pass!" });
+    }
+  })
+  .catch((error) => {
+    res.status(500).json({ message: "Mystery Error!", error});
+  });
+});
+
+router.get("/users", (req, res) => {
+  dbFun.getUsers()
   .then((dbRes) => {
     res.status(200).json(dbRes);
   })
   .catch((dbErr) => {
     res.status(500).json(dbErr);
   });
-});
-
-// router.post('/login', (req, res) => {
-//   let { username, password } = req.body;
-
-//   dbFun.findByUsername(username)
-//   .then((user)) => 
-// });
+})
 
 module.exports = router;
